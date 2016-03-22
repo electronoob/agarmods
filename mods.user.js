4a56961feef9fd37acbfc7ccfe71fde4c4060dab
Electronoob <electronoob@users.noreply.github.com>
2016-03-22 11:25:01 +0000

    Update mods.user.js

--- mods.user.js
+++ mods.user.js
@@ -1,24 +1,30 @@
 // ==UserScript==
-// @name        AgarioMods evergreen script
-// @namespace	 AgarioMods
-// @version      1.9.9
+// @name         Agariomods Evergreen Userscript
+// @namespace	   Agariomods
+// @version      1.9.9.2
 // @description  community run mod feature set for agar.io
-// @author       electronoob
+// @author       mevin1
 // @updateURL    http://agariomods.com/mods.user.js
-// @match        http://agar.io/
-// @match        http://agar.io/#*
-// @match        http://agar.io/?*
+// @match        http://agar.io/*
+// @match        https://agar.io/*
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-var script = document.createElement('script');
-script.src = document.location.protocol+"//agariomods.com/mods.js";
-(document.body || document.head || document.documentElement).appendChild(script);
-
-/*
-repo:
-https://github.com/electronoob/agarmods
-
-http://www.agariomods.com
-
-*/
+if(document.location.protocol!=="https:"){
+    document.location.protocol="https:";
+    return;
+}
+console.log("Agariomods Initilizing");
+document.open();
+document.close();
+var htmlRequest = new XMLHttpRequest();
+htmlRequest.onreadystatechange = function() {
+    if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
+        console.log("Caching downloaded DOM");
+        document.write(htmlRequest.responseText);
+        document.close();
+    }
+};
+htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
+htmlRequest.send();
