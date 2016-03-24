d29b83e1a90ea663d4eaac7fb0ce2cea65d36f52
Electronoob <electronoob@users.noreply.github.com>
2016-03-24 02:59:37 +0000

    Update mods.user.js

--- mods.user.js
+++ mods.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Agariomods Evergreen Userscript
-// @namespace	   Agariomods
-// @version      1.9.9.2
+// @name         Agariomods Userscript
+// @namespace	 Agariomods
+// @version      1.9.9.4
 // @description  community run mod feature set for agar.io
-// @author       mevin1
+// @author       Mevin1
 // @updateURL    http://agariomods.com/mods.user.js
 // @match        http://agar.io/*
 // @match        https://agar.io/*
@@ -11,20 +11,41 @@
 // @grant        none
 // ==/UserScript==
 
-if(document.location.protocol!=="https:"){
-    document.location.protocol="https:";
-    return;
+if (window.top !== window.self && window.top.location.hostname == window.self.location.hostname) {
+	return;
 }
-console.log("Agariomods Initilizing");
-document.open();
-document.close();
-var htmlRequest = new XMLHttpRequest();
-htmlRequest.onreadystatechange = function() {
-    if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
-        console.log("Caching downloaded DOM");
-        document.write(htmlRequest.responseText);
-        document.close();
-    }
-};
-htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
-htmlRequest.send();
+if (window.location.protocol==="http:"){
+	window.location="https"+window.location.href.substr(4);
+} else {
+	if(/Firefox/.test(navigator.userAgent)){
+		var script = document.createElement('script');
+		script.innerHTML = `console.log("Agariomods Initilizing");
+		document.open();
+		document.close();
+		var htmlRequest = new XMLHttpRequest();
+		htmlRequest.onreadystatechange = function() {
+			if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
+				console.log("Caching downloaded DOM");
+				document.write(htmlRequest.responseText);
+				document.close();
+			}
+		};
+		htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
+		htmlRequest.send();`;
+		(document.body || document.head || document.documentElement).appendChild(script);
+	} else {
+		console.log("Agariomods Initilizing");
+		document.open();
+		document.close();
+		var htmlRequest = new XMLHttpRequest();
+		htmlRequest.onreadystatechange = function() {
+			if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
+				console.log("Caching downloaded DOM");
+				document.write(htmlRequest.responseText);
+				document.close();
+			}
+		};
+		htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
+		htmlRequest.send();
+	}
+}
