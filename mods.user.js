f467b4db398a51a2c2b7d303516b8779b514c744
Electronoob <electronoob@users.noreply.github.com>
2016-04-19 02:52:04 +0100

    Update mods.user.js

--- mods.user.js
+++ mods.user.js
@@ -1,40 +1,43 @@
 // ==UserScript==
-// @name         Agariomods Userscript
-// @namespace	 Agariomods
-// @version      1.9.9.4
-// @description  community run mod feature set for agar.io
+// @name         Iomods
+// @namespace	 Iomods
+// @version      2.0
+// @description  community run mod feature set for agar.io and slither.io
 // @author       Mevin1
-// @updateURL    http://agariomods.com/mods.user.js
+// @match        http://slither.io/*
+// @match        https://slither.io/*
 // @match        http://agar.io/*
 // @match        https://agar.io/*
+// @updateURL    https://iomods.com/mods.user.js
 // @run-at       document-start
 // @grant        none
+// @noframes
 // ==/UserScript==
 
-if (window.top !== window.self && window.top.location.hostname == window.self.location.hostname) {
-	return;
-}
-if (window.location.protocol==="http:"){
+if (window.location.hostname=="agar.io"&&window.location.protocol==="http:"){
 	window.location="https"+window.location.href.substr(4);
-} else {
+} else if (~["agar.io","slither.io"].indexOf(window.location.hostname)) {
 	if(/Firefox/.test(navigator.userAgent)){
 		var script = document.createElement('script');
-		script.innerHTML = `console.log("Agariomods Initilizing");
-		document.open();
-		document.close();
-		var htmlRequest = new XMLHttpRequest();
-		htmlRequest.onreadystatechange = function() {
-			if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
-				console.log("Caching downloaded DOM");
-				document.write(htmlRequest.responseText);
-				document.close();
-			}
-		};
-		htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
-		htmlRequest.send();`;
-		(document.body || document.head || document.documentElement).appendChild(script);
+		script.innerHTML = `console.log("Iomods Initilizing");
+			document.open();
+			document.close();
+			var htmlRequest = new XMLHttpRequest();
+			htmlRequest.onreadystatechange = function() {
+				if (htmlRequest.readyState == 4 && htmlRequest.status == 200) {
+					document.onreadystatechange = function(){
+						if(document.readyState!="complete")return;
+						console.log("Caching downloaded DOM");
+						document.write(htmlRequest.responseText);
+						document.close();
+					}
+				}
+			};
+			htmlRequest.open("GET", "https://${window.location.hostname}mods.com/mod/index.html", true);
+			htmlRequest.send();`;
+		document.head.appendChild(script);
 	} else {
-		console.log("Agariomods Initilizing");
+		console.log("Iomods Initilizing");
 		document.open();
 		document.close();
 		var htmlRequest = new XMLHttpRequest();
@@ -45,7 +48,7 @@ if (window.location.protocol==="http:"){
 				document.close();
 			}
 		};
-		htmlRequest.open("GET", "https://connect.agariomods.com/a/index.html", true);
+		htmlRequest.open("GET", `https://${window.location.hostname}mods.com/mod/index.html`, true);
 		htmlRequest.send();
 	}
 }
